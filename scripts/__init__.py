9ff2c6d3bf036f8a27435ccc77ac2bb355c3757d
bengriffy <bengriffy@gmail.com>
2016-11-30 13:19:34 -0800

    Add files via upload

---
+++ scripts/__init__.py
@@ -0,0 +1,7 @@
+################################################################################################################
+#Author: Ben Griffy
+#Institution: University of California, Santa Barbara
+#email: griffy@umail.ucsb.edu
+#website: https://sites.google.com/site/bengriffy/home
+#Date:
+################################################################################################################
